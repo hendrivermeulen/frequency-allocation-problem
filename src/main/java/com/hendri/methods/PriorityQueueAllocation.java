02532b49999895ece3efcc037895f76e6af32305
Hendrik Vermeulen <hendrik@peralex.com>
2025-06-14 23:24:15 +0200

    Priority Method added

---
+++ src/main/java/com/hendri/methods/PriorityQueueAllocation.java
@@ -0,0 +1,83 @@
+package com.hendri.methods;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.hendri.models.Cell;
+import com.hendri.models.Edge;
+import com.hendri.models.RegisteredCell;
+import com.hendri.utils.Graph;
+
+public class PriorityQueueAllocation
+{
+
+	public static Set<RegisteredCell> priorityAllocate(Set<Cell> cells, Set<Integer> frequencies){
+		Comparator<Edge> edgeComparator = Comparator.comparingDouble(
+				edge -> Graph.getDistanceBetween(edge.getFrom(), edge.getTo()));
+
+		HashMap<Cell, PriorityQueue<Edge>> allCellPriorities = new HashMap<>();
+		PriorityQueue<Edge> edgePriorities = new PriorityQueue<>(edgeComparator);
+
+		for (Cell cell : cells) {
+			PriorityQueue<Edge> cellPriorities = new PriorityQueue<>(edgeComparator);
+			for (Cell neighbour : cells) {
+				if(!cell.equals(neighbour)){
+					Edge edge = Edge.builder().from(cell).to(neighbour).build();
+					cellPriorities.add(edge);
+					edgePriorities.add(edge);
+				}
+			}
+			allCellPriorities.put(cell, cellPriorities);
+		}
+
+		HashMap<Cell, Integer> allocations = new HashMap<>();
+		while (!edgePriorities.isEmpty()){
+			Edge edge = edgePriorities.poll();
+			allocations.computeIfAbsent(edge.getFrom(), cell -> allocate(cell,
+					allCellPriorities.get(cell), allocations, frequencies));
+			allocations.computeIfAbsent(edge.getTo(), cell -> allocate(cell,
+					allCellPriorities.get(cell), allocations, frequencies));
+		}
+
+		return allocations.entrySet().stream().map(pair->
+				RegisteredCell.builder().cell(pair.getKey()).frequency(pair.getValue()).build())
+				.collect(Collectors.toSet());
+	}
+
+	private static int allocate(Cell cell, PriorityQueue<Edge> cellPriorities,
+			HashMap<Cell, Integer> allocations, Set<Integer> freqAllocations){
+		int furthestAwayFreq = -1;
+		double furthestAwayFreqDistance = -1;
+		Set<Integer> leftoverAllocations = new HashSet<>(freqAllocations);
+
+		if(allocations.values().size() > freqAllocations.size()) {
+			while (!cellPriorities.isEmpty() && !leftoverAllocations.isEmpty()) {
+				Edge edge = cellPriorities.poll();
+				Cell neighbour = (edge.getFrom().equals(cell)) ? edge.getTo() : edge.getFrom();
+				if (allocations.containsKey(neighbour)) {
+					Integer freqAllocation = allocations.get(neighbour);
+					leftoverAllocations.remove(freqAllocation);
+					double distanceBetween = Graph.getDistanceBetween(cell, neighbour);
+					if (furthestAwayFreqDistance < distanceBetween) {
+						furthestAwayFreq = freqAllocation;
+						furthestAwayFreqDistance = distanceBetween;
+					}
+				}
+			}
+		} else {
+			for (Integer integer : allocations.values()) {
+				leftoverAllocations.remove(integer);
+			}
+		}
+
+		if(leftoverAllocations.isEmpty()){
+			return furthestAwayFreq;
+		}
+		return leftoverAllocations.stream().findFirst().get();
+	}
+
+}
---
+++ src/test/java/com/hendri/methods/PriorityQueueAllocationTest.java
@@ -0,0 +1,61 @@
+package com.hendri.methods;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import com.hendri.models.Cell;
+import com.hendri.models.RegisteredCell;
+import com.hendri.utils.Graph;
+import org.junit.jupiter.api.Test;
+import static com.hendri.utils.TestData.A;
+import static com.hendri.utils.TestData.B;
+import static com.hendri.utils.TestData.C;
+import static com.hendri.utils.TestData.FREQUENCIES;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PriorityQueueAllocationTest
+{
+
+	@Test
+	void priorityNoInterference(){
+		Set<Cell> testData = Set.of(A, B, C);
+		Set<RegisteredCell> actual = PriorityQueueAllocation.priorityAllocate(testData, FREQUENCIES);
+
+		Iterator<RegisteredCell> iterator = actual.iterator();
+		RegisteredCell first = iterator.next();
+		RegisteredCell second = iterator.next();
+		RegisteredCell third = iterator.next();
+
+		assertEquals(100, Graph.getGraphQualityMeasure(actual).getQuality());
+		assertNotEquals(first.getFrequency(), second.getFrequency());
+		assertNotEquals(first.getFrequency(), third.getFrequency());
+		assertNotEquals(second.getFrequency(), third.getFrequency());
+	}
+
+	@Test
+	void priorityPredictiveInterference(){
+		Cell cellA = Cell.builder().id("A").lat(0.0).lon(0.0).north(0).east(0).build();
+		Cell cellB = Cell.builder().id("B").lat(0.0).lon(0.0).north(0).east(0).build();
+		Cell cellC = Cell.builder().id("C").lat(0.0).lon(0.0).north(0).east(0).build();
+		Cell cellD = Cell.builder().id("D").lat(0.0).lon(0.0).north(100).east(100).build();
+
+		Set<RegisteredCell> actual = PriorityQueueAllocation.priorityAllocate(
+				Set.of(cellA, cellB, cellC, cellD), FREQUENCIES);
+
+		assertEquals(100, Graph.getGraphQualityMeasure(actual).getQuality());
+	}
+
+	@Test
+	void priorityDualInterference(){
+		Cell cellA = Cell.builder().id("A").lat(0.0).lon(0.0).north(0).east(0).build();
+		Cell cellB = Cell.builder().id("B").lat(0.0).lon(0.0).north(0).east(0).build();
+		Cell cellC = Cell.builder().id("C").lat(0.0).lon(0.0).north(0).east(0).build();
+		Cell cellD = Cell.builder().id("D").lat(0.0).lon(0.0).north(100).east(100).build();
+		Cell cellE = Cell.builder().id("E").lat(0.0).lon(0.0).north(100).east(100).build();
+
+		Set<RegisteredCell> actual = PriorityQueueAllocation.priorityAllocate(
+				Set.of(cellA, cellB, cellC, cellD, cellE), FREQUENCIES);
+
+		assertEquals(100, Graph.getGraphQualityMeasure(actual).getQuality());
+	}
+}
